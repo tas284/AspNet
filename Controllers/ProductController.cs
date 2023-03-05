c0f74b83d6169c21d9b11a5f274e56abb6e6362e
tas284 <tiago.avila.saldanha@gmail.com>
2023-03-05 15:58:13 -0300

    Revert return of UpdateProduct

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -38,7 +38,7 @@ public class ProductController : ControllerBase
             product.Id = id;
             
             await _productRepository.UpdateOneAsync(id, product);
-            return Ok(product);
+            return Ok($"Product updated successfully! Id: {id}");
         }
         catch(Exception ex){
             return BadRequest(ex.Message);
