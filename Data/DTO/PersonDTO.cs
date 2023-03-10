df8b95e0e4ea97c3350b2521419604365faeed71
tas284 <tiago.avila.saldanha@gmail.com>
2023-03-09 23:25:33 -0300

    Add type Person

--- Data/DTO/PersonDTO.cs
+++ Data/DTO/PersonDTO.cs
@@ -7,6 +7,7 @@ public class PersonDTO
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Phone { get; set; }
+    public string? type { get; set; }
     [JsonIgnore]
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -9,4 +9,5 @@ public class Person : Document
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Phone { get; set; }
+    public string? type { get; set; }
 }
