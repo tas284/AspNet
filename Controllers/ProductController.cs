1ae415e409f0e39f29c8c2abcac79fb2c1228951
tas284 <tiago.avila.saldanha@gmail.com>
2023-08-12 19:13:37 -0300

    Update return of update method on ProductController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -39,7 +39,7 @@ public class ProductController : ControllerBase
             product.Id = id;
 
             await _productRepository.UpdateOneAsync(id, product);
-            return Ok($"Product updated successfully! Id: {id}");
+            return Ok(product);
         }
         catch (Exception ex)
         {
